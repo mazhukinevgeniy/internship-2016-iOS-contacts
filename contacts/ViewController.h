f299344e2a435da98df4bc7026912197744dfe72
mazhukin evgeniy <mazhukin.evgeniy@gmail.com>
2016-11-01 00:13:47 +0300

    Add empty Xcode project

---
+++ contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  contacts
+//
+//  Created by Admin on 01/11/16.
+//  Copyright © 2016 noname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
