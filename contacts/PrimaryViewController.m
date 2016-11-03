216f3f6460cd6001d3ed8d5c98693e3a0cf2197a
mazhukin evgeniy <mazhukin.evgeniy@gmail.com>
2016-11-03 14:40:21 +0300

    Add sample contacts

--- contacts/PrimaryViewController.m
+++ contacts/PrimaryViewController.m
@@ -9,17 +9,41 @@
 #import "PrimaryViewController.h"
 #import "ContactStorage.h"
 
+@interface PrimaryViewController()
+
+- (void)addSampleContacts:(ContactStorage *)storage;
+
+@end
+
+
+
 @implementation PrimaryViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     ContactStorage * storage = [[ContactStorage alloc] init];
-    //TODO: add some contacts
+    [self addSampleContacts:storage];
     
     for (NSObject<ContactStorageUser>* controller in [self viewControllers])
     {
         [controller useContactStorage:storage];
+    }
+}
+
+- (void)addSampleContacts:(ContactStorage *)storage {
+    NSArray * ids = @[@0, @1, @2, @3];
+    NSArray * names = @[@"Bob", @"", @"Disable roaming", @"Sara"];
+    NSArray * lastNames = @[@"", @"Vasiliev", @"", @"Poppins"];
+    NSArray * numbers = @[@"89123", @"01", @"#101*", @"89654"];
+    
+    for (int i = 0; i < 4; i++)
+    {
+        Contact * contact = [Contact initWith:(int)ids[i] firstName:names[i]
+                                    lastName:lastNames[i]
+                                       number:numbers[i]];
+        
+        [storage addContact:contact];
     }
 }
 
